a922d427c4aedcb66f714382d7dec07e3c0e30fd
savannah1442 <doriss45@poczta.onet.pl>
2016-11-01 16:48:47 +0100

    Add project files.

---
+++ nowy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nowy.MvcApplication" Language="C#" %>
---
+++ nowy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nowy.Startup))]
+namespace nowy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
